455e6a7189f1126f6bba5d8bcd69262b51ffd6f2
mmihira <mmihira.w@gmail.com>
2019-03-07 16:08:42 +1100

    Also calculate sharip hash

--- src/wallet/key.go
+++ src/wallet/key.go
@@ -10,12 +10,14 @@ import (
 	"math/big"
 )
 
+// Key Ecdsa pub/priv key
 type Key struct {
 	PrivateKeyInt       *big.Int
 	PrivateKeyBytes     []byte
 	PrivateKeyHexString string
 	PublicKeyX          *big.Int
 	PublicKeyXBytes     []byte
+	PublicKeyHash 			[]byte
 	BtcAddressBytes     []byte
 	BtcAddressString    string
 }
@@ -33,15 +35,21 @@ func NewKey() Key {
 	return keyFromLibPrivKey(newPrivKey)
 }
 
+// sha256 of the byte buffer followed by ripemd160
+func ripe160sha256(b []byte) []byte {
+	sha := sha256.Sum256(b)
+	h := ripemd160.New()
+	h.Write(sha[:])
+	return h.Sum(nil)
+}
+
 // keyFromLibPrivKey Create a Key from a btcec.PrivateKey
 func keyFromLibPrivKey(k *btcec.PrivateKey) Key {
 	PubKBytes := []byte{0x02}
+	// (33 bytes, 1 byte 0x02 (y-coord is even), and 32 bytes corresponding to X coordinate)
 	PubKBytes = append(PubKBytes, k.PublicKey.X.Bytes()...)
-	PubKSHA256Bytes := sha256.Sum256(PubKBytes)
-
-	h := ripemd160.New()
-	h.Write(PubKSHA256Bytes[:])
-	PubKRipeMDBytes := h.Sum(nil)
+	PubKRipeMDBytes := ripe160sha256(PubKBytes)
+	// Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
 	PubKRipeMDBytes = append([]byte{0x00}, PubKRipeMDBytes...)
 	PubKRipeMDBytesSha256 := sha256.Sum256(PubKRipeMDBytes)
 	PubKRipeMDBytesSha256 = sha256.Sum256(PubKRipeMDBytesSha256[:])
@@ -56,6 +64,7 @@ func keyFromLibPrivKey(k *btcec.PrivateKey) Key {
 		PrivateKeyHexString: hex.EncodeToString(k.D.Bytes()[:]),
 		PublicKeyX:          k.PublicKey.X,
 		PublicKeyXBytes:     k.PublicKey.X.Bytes(),
+		PublicKeyHash: 		   ripe160sha256(k.PublicKey.X.Bytes()),
 		BtcAddressBytes:     BtcAddressBytes,
 		BtcAddressString:    base58.Encode(BtcAddressBytes),
 	}
